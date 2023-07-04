4719b080226c67b4e114b9fa04d442529c872dc4
Izabely Furtado <izabely_furtado@hotmail.com>
2023-07-04 14:43:29 -0300

    criando projeto front - casca

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  constructor(private config: PrimeNGConfig, private translateService: TranslateService) {}
+  title = 'mangata';
+  ngOnInit() {
+    this.config.ripple = true;
+    this.translateService.setDefaultLang('pt-BR');
+  }
+
+  translate(lang: string) {
+    this.translateService.use(lang);
+    this.translateService.get('primeng').subscribe(res => this.config.setTranslation(res));
+  }
+}
