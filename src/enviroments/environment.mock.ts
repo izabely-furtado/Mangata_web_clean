16d0f9a1f3b2344ea7ca814166dc4d74345051d2
Izabely Furtado <izabely_furtado@hotmail.com>
2023-08-16 11:05:33 -0300

    Revert "testando realocando projeto"
    This reverts commit d8413a89f4e72cdfe68a81814daaba34b8a283db.

--- src/enviroments/environment.mock.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    mock: true,
-    production: false,
-    apiUrl: 'https://api.example.com',
-    apiUrlEstados: 'http://localhost:3000/estados'
-    // Outras variáveis de ambiente específicas para o ambiente de produção
-  };
-  
